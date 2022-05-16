99988369d3a78d0394aeed795444a3072f226aa2
ikedark <73199797+ikedark@users.noreply.github.com>
2022-05-16 14:25:51 -0700

    src

---
+++ src/client/ClienteHilo.java
@@ -0,0 +1,58 @@
+package client;
+
+import frames.Login;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClienteHilo extends Thread {
+    private static String mensaje;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+    public static int numJugador;
+
+    public ClienteHilo() {
+        
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            
+            try {
+                mensaje=in.readUTF();
+                if(mensaje.startsWith("nombre ")){
+                    System.out.println(mensaje.substring(7));
+                }else if(mensaje.startsWith("num ")){
+                    numJugador=Integer.parseInt(mensaje.substring(4));
+                //Cuando llegue un mensaje que empiece con el número del jugador
+                }else if(mensaje.startsWith(String.valueOf(numJugador))){
+                    
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ClienteHilo.class.getName()).log(Level.SEVERE, null, ex);
+            }catch (NullPointerException ne){
+                
+            }
+
+        }
+
+    }
+    
+    public static void enviarAlServidor(String mensaje) throws IOException{
+        out.writeUTF(mensaje);
+    }
+    
+    public static void entrarAlServidor(String direccion) throws IOException{
+        Socket sc = new Socket(direccion, 5000);
+        DataInputStream in = new DataInputStream(sc.getInputStream());
+        DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+        ClienteHilo.in = in;
+        ClienteHilo.out=out;
+    }
+    
+}
